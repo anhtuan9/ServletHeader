75881cb7c341c04f63fa1f7bbdc963474c012faa
anhtuan9 <42605422+anhtuan9@users.noreply.github.com>
2018-10-19 10:33:11 +0700

    first-commit

---
+++ src/main/java/ServletHeader.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ServletHeader extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        String userAgent = request.getHeader("User-Agent");
+        PrintWriter writer = response.getWriter();
+        String browser = "Unknow browser";
+        if (userAgent.contains("Firefox")) {
+            browser = "Mozilla Firefox";
+        } else if (userAgent.contains("OPR")) {
+            browser = "Opera";
+        } else if (userAgent.contains("Safari")) {
+            if (userAgent.contains("Chrome")) {
+                browser = "Google Chrome";
+            } else {
+                browser = "Apple Safari";
+            }
+        }
+
+        writer.println("<html>");
+        writer.println("<h1>Browser Detector</h1>");
+        writer.println("<h3>Your User-Agent Is: " + userAgent + "</h3>");
+        writer.println("<h3>Your Browser Is: " + browser + "</h3>");
+        writer.println("</html>");
+    }
+}
